45070e71e64b0e83df5d3d9f301156b8b68005c3
Mokongchung <mokongchung1@gmail.com>
2025-05-07 11:30:05 +0700

    ~ Rename project cocos

---
+++ Moko_02_K5/assets/Day1/Bai7/Data_17/MainScript_17.js
@@ -0,0 +1,28 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: { 
+        btnPower    :cc.Button,
+        lblPewer    :cc.Label,
+        inputHP     :cc.EditBox,
+        inputMP     :cc.EditBox,
+        
+    },
+
+
+    
+    onLoad () {
+        
+    },
+
+    start () {
+        
+    },
+
+    onBtnAtkClick(){
+    
+    }
+
+    // update (dt) {},
+});
